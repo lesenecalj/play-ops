19d7f708b2e2b0aaf321024c62a2828605e8c9c5
jeremy.lesenecal <lesenecal.jeremy@rcgt.com>
2025-08-30 13:52:24 -0400

    feat(init): initiate project with https://start.spring.io
    feat(init): initiate project with https://start.spring.io

---
+++ src/main/java/com/playops/PlayOpsApplication.java
@@ -0,0 +1,13 @@
+package com.playops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playops/PlayOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
